ea99c80900da98358042335945fcfebce4527218
Kurtis Mash <Kurtis.Mash@bbc.co.uk>
2020-04-06 12:21:36 +0100

    Add shared libraries for pre-signed S3 URL

--- README.md
+++ README.md
@@ -14,11 +14,23 @@ This compiles Mediainfo with libcurl so that it can be useful with URLs.
 
 `./compile.sh`
 
-This produces two files in you current working directory:
+This produces 14 files in you current working directory:
 
 * `mediainfo`
+* `libcrypt.so.1`
 * `libcurl.so.4`
+* `libidn2.so.0`
+* `liblber-2.4.so.2`
+* `libldap-2.4.so.2`
+* `libnghttp2.so.14`
+* `libnss3.so`
+* `libnssutil3.so`
+* `libsasl2.so.3`
+* `libsmime3.so`
+* `libssh2.so.1`
+* `libssl3.so`
+* `libunistring.so.0`
 
 ### Usage
 
-If you place `libcurl.so.4` in the base directory of the Lambda, it will be found by the `LD_LIBRARY_PATH` of the execution environment. After that, you can run `mediainfo` from just about anywhere.
+If you place the library files (`*.so.*`) in the base directory of the Lambda, they will be found by the `LD_LIBRARY_PATH` of the execution environment. After that, you can run `mediainfo` from just about anywhere.
--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t aws-mediainfo .
-docker run -it --rm -v $PWD:/tmp aws-mediainfo bash -c 'cp /root/MediaInfo_CLI_GNU_FromSource/mediainfo /tmp; cp /root/MediaInfo_CLI_GNU_FromSource/libcurl.so.4 /tmp'
+docker run -it --rm -v $PWD:/tmp aws-mediainfo bash -c 'cp /root/MediaInfo_CLI_GNU_FromSource/mediainfo /tmp; cp /root/MediaInfo_CLI_GNU_FromSource/libcurl.so.4 /tmp; cp /root/MediaInfo_CLI_GNU_FromSource/libcrypt.so.1 /tmp; cp /root/MediaInfo_CLI_GNU_FromSource/libidn2.so.0 /tmp; cp /root/MediaInfo_CLI_GNU_FromSource/liblber-2.4.so.2 /tmp; cp /root/MediaInfo_CLI_GNU_FromSource/libldap-2.4.so.2 /tmp; cp /root/MediaInfo_CLI_GNU_FromSource/libnghttp2.so.14 /tmp; cp /root/MediaInfo_CLI_GNU_FromSource/libnss3.so /tmp; cp /root/MediaInfo_CLI_GNU_FromSource/libnssutil3.so /tmp; cp /root/MediaInfo_CLI_GNU_FromSource/libsasl2.so.3 /tmp; cp /root/MediaInfo_CLI_GNU_FromSource/libsmime3.so /tmp; cp /root/MediaInfo_CLI_GNU_FromSource/libssh2.so.1 /tmp; cp /root/MediaInfo_CLI_GNU_FromSource/libssl3.so /tmp; cp /root/MediaInfo_CLI_GNU_FromSource/libunistring.so.0 /tmp'
